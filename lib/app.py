049df256f6fedb6ca0b9fc574f80a42bde90fe62
Lewis <09lewismtwi@gmail.com>
2024-08-04 19:11:23 +0300

    save

---
+++ lib/app.py
@@ -0,0 +1,14 @@
+# lib/app.py
+
+print("Hello World! Pass this test, please.");
+dog_name = "Lucy"
+print(f"my dogs name is {dog_name}")
+list = ['A' , 'B' , 'C']
+list.append('D')
+print(list)
+list.pop()
+print(list)
+list.sort()
+print(list)
+list.insert(0, "Baby")
+print(list)
