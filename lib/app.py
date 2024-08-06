41ea3f163e0bce37af5d653e3a9aa8abca1d6650
Lewis <09lewismtwi@gmail.com>
2024-08-06 09:54:03 +0300

    guessgame

--- lib/app.py
+++ lib/app.py
@@ -12,3 +12,26 @@ list.sort()
 print(list)
 list.insert(0, "Baby")
 print(list)
+import random
+
+def guess_the_number():
+    print("Welcome to 'Guess the Number'!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    number_to_guess = random.randint(1, 100)
+    guess = None
+    number_of_guesses = 0
+    
+    while guess != number_to_guess:
+        guess = int(input("Take a guess: "))
+        number_of_guesses += 1
+        
+        if guess < number_to_guess:
+            print("Your guess is too low.")
+        elif guess > number_to_guess:
+            print("Your guess is too high.")
+        else:
+            print(f"Congratulations! You guessed the number in {number_of_guesses} tries.")
+            
+if __name__ == "__main__":
+    guess_the_number()
